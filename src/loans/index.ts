9e76387b3f1022e15d6de4c21a6628be9438baac
Rustam Tairov <rustairov@gmail.com>
2022-08-24 16:51:16 +0100

    Export

--- src/loans/index.ts
+++ src/loans/index.ts
@@ -28,5 +28,7 @@ export * from './functions/public/paybackLoanWithGrace';
 export * from './functions/public/stakeGemFarm';
 export * from './functions/public/unstakeGemFarm';
 export * from './functions/public/claimGemFarm';
+export * from './functions/public/calculateRewardDegod';
+export * from './functions/public/getGemFarmAccount';
 
 export * from './helpers';
