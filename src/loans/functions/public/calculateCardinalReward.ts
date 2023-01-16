22a9c4b905ca70971045af3149b0fcdd00e33921
Valpaq <viktor.paramonov2000@gmail.com>
2023-01-16 20:59:41 +0300

    typo in math and stake updated

--- src/loans/functions/public/calculateCardinalReward.ts
+++ src/loans/functions/public/calculateCardinalReward.ts
@@ -1,4 +1,5 @@
 import  { StakeEntryView, RewardDistributorView, RewardEntryView } from '../../types';
+import {BN} from "@project-serum/anchor";
 
 type CalculationRewardCardinal = (params: {
     stakeEntry: StakeEntryView, 
@@ -8,17 +9,55 @@ type CalculationRewardCardinal = (params: {
 }) => number;
 
 export const calculationRewardCardinal: CalculationRewardCardinal = ({
-    stakeEntry,
-    rewardDistributor,
-    rewardEntry,
-    currentSeconds
-}) => {   
-  if (stakeEntry.amount !== 1) {
+  stakeEntry,
+  rewardDistributor,
+  rewardEntry,
+  currentSeconds
+}) => {  
+  if (
+    !stakeEntry ||
+    stakeEntry.pool.toString() !==
+      rewardDistributor.stakePool.toString()
+  ) {
     return 0;
   }
 
-  let rewardSeconds = (currentSeconds - stakeEntry.lastUpdatedAt) * stakeEntry.amount + stakeEntry.totalStakeSeconds
+  const rewardSecondsReceived =
+    rewardEntry.rewardSecondsReceived || new BN(0);
+  const multiplier =
+    rewardEntry?.multiplier ||
+    rewardDistributor.defaultMultiplier;
+  const UTCNow = stakeEntry.cooldownStartSeconds
+    ? new BN(stakeEntry.cooldownStartSeconds)
+    : new BN(currentSeconds);
 
-  let rewardAmountToReceive = Math.floor(rewardSeconds * rewardDistributor.rewardAmount * (rewardEntry.multiplier) / (10 ** rewardDistributor.multiplierDecimals))
+  let rewardSeconds = UTCNow
+    .sub(new BN(stakeEntry.lastStakedAt))
+    .mul(new BN(stakeEntry.amount))
+    .add(new BN(stakeEntry.totalStakeSeconds));
+
+  if (rewardDistributor.maxRewardSecondsReceived) {
+    rewardSeconds = BN.min(
+      rewardSeconds,
+      new BN(rewardDistributor.maxRewardSecondsReceived)
+    );
+  }
+
+  const rewardAmountToReceive = rewardSeconds
+    .sub(new BN(rewardSecondsReceived))
+    .div(new BN(rewardDistributor.rewardDurationSeconds))
+    .mul(new BN(rewardDistributor.rewardAmount))
+    .mul(new BN(multiplier))
+    .div(new BN(10).pow(new BN(rewardDistributor.multiplierDecimals))).toNumber();
+
+  const nextRewardsIn = new BN(
+    rewardDistributor.rewardDurationSeconds
+  ).sub(
+    UTCNow
+      .sub(new BN(stakeEntry.lastStakedAt))
+      .add(new BN(stakeEntry.totalStakeSeconds))
+      .mod(new BN(rewardDistributor.rewardDurationSeconds))
+  );
+
   return rewardAmountToReceive;
 };
